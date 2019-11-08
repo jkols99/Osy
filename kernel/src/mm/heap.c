4e252075b4f31996e85cfd7c13983a94aea0bbdc
Adam <adam.dafcik@gmail.com>
2019-11-08 18:31:07 +0100

    Add malloc implementation

--- kernel/src/mm/heap.c
+++ kernel/src/mm/heap.c
@@ -1,11 +1,32 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright 2019 Charles University
 
+#include <main.h>
 #include <mm/heap.h>
+#include <debug/code.h>
+typedef struct Item {
+    struct Item* next;
+    size_t mem_amount;
+} mem_chunk;
+
+mem_chunk* head;
 
 void* kmalloc(size_t size) {
-    return NULL;
+    mem_chunk* temp = head;
+    while (temp->next) {
+        temp = temp->next;
+    }
+    mem_chunk* new_mem;
+    new_mem->mem_amount = size;
+    temp->next = new_mem;
+    uintptr_t address = debug_get_stack_pointer();
+    __asm__ volatile("addi $29 $29 %1;" : "r"(size));
+    return address;
 }
 
 void kfree(void* ptr) {
+}
+
+void heap_init() {
+    head->mem_amount = 0;
 }
